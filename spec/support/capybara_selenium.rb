6881d27121f17cc26edb67003fe816a069ee70a0
Namibia Torres <namibialt@gmail.com>
2018-03-19 13:40:11 +1100

    added shoulda_matchers configuration

---
+++ spec/support/capybara_selenium.rb
@@ -0,0 +1,18 @@
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.javascript_driver = :chrome
+
+Capybara.configure do |config|
+  config.default_max_wait_time = 10 # seconds
+  config.default_driver        = :selenium
+end
+
+RSpec.configure do |config|
+  # ...
+  config.before(:each, type: :feature) do
+    # Note (Mike Coutermarsh): Make browser huge so that no content is hidden during tests
+    Capybara.current_session.driver.browser.manage.window.resize_to(2_500, 2_500)
+  end
+end
