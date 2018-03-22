0266333dda58bf4a5997770335083acd72563af4
Namibia Torres <namibialt@gmail.com>
2018-03-22 11:39:58 +1100

    Configured show action and show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,5 +4,28 @@ class BooksController < ApplicationController
     @books = Book.published
   end
 
+  def new
+    @books = Book.new
+  end
 
+  def create
+    @books = Book.new(book_params)
+
+    if @books.save
+      flash[:notice] = 'Book has been created.'
+      redirect_to @books
+    else
+      #nothing yet
+    end
+  end
+
+  def show
+    @books = Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description)
+  end
 end
