0ad3438fff4fab239e6e56cffbb1306e4f2bb0e6
Namibia Torres <namibialt@gmail.com>
2018-03-23 14:36:45 +1100

    Refactored code in spec file

--- spec/features/creating_books_spec.rb
+++ spec/features/creating_books_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature 'Users can create their own books' do
-  scenario 'with valid attributes' do
+
+  def create_book
     visit root_path
 
     click_on 'New Book'
@@ -10,16 +11,21 @@ RSpec.feature 'Users can create their own books' do
     fill_in 'Description', with: 'Non-fiction'
     fill_in 'Summary', with: 'A book about housing in NYC.'
     click_on 'Create Book'
+  end
+
+  scenario 'with valid attributes' do
+    create_book
 
     expect(page).to have_content 'Book has been created.'
   end
 
   scenario 'title is correctly implemented' do
-    pending "Not implemented yet"
-    book = Book.find_by(title: 'Jindle')
-    expect(page.current_url).to eq book_url(book)
+    create_book
 
-    title = 'Your Books'
+    book = Book.find_by(title: 'Soho')
+    expect(page.current_path).to eq book_path(book)
+
+    title = 'Jindle'
     expect(page).to have_title title
   end
 end
