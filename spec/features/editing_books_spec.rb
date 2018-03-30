3c2392d8ad52c3d301e57c527b95c1b060c5c5a4
Namibia Torres <namibialt@gmail.com>
2018-03-30 22:26:22 +1100

    added unhappy path spec

--- spec/features/editing_books_spec.rb
+++ spec/features/editing_books_spec.rb
@@ -1,17 +1,25 @@
 require 'rails_helper'
 
 RSpec.feature 'Users can edit the books they have created' do
-
-  scenario 'when attributes are valid' do
+  before do
     FactoryBot.create(:book, title: 'Hello')
 
     visit root_path
-    click_on 'Hello'
+    # click_on 'Hello'
     click_on 'Edit Book'
+  end
+  scenario 'when attributes are valid' do
     fill_in 'Title', with: 'Hello, World!'
     click_on 'Update Book'
 
     expect(page).to have_content 'Your book has been updated'
     expect(page).to have_content 'Hello, World!'
+  end
+
+  scenario 'when attributes are invalid' do
+    fill_in 'Title', with: ''
+    click_button 'Update Book'
+
+    expect(page).to have_content 'Book has not been updated'
   end
 end
