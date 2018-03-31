25a31e7a1732117a5fb38ce19b4880571e6a5fb1
Namibia Torres <namibialt@gmail.com>
2018-03-31 17:34:30 +1100

    commented out static asset directory command

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Serve static assets from a public directory:
-  config.serve_static_file = true
+  # config.serve_static_file = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
