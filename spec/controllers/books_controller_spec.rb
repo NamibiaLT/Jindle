5dda7388afc50ec3f2eac8976fb9ccc999d4ebf9
Namibia Torres <namibialt@gmail.com>
2018-03-31 17:35:39 +1100

    Test for redirecting users to the book page if they attempt to find a book that does not exist

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
+  it 'handles a missing book properly' do
+    get :show, id: 'not-here'
 
+    expect(response).to redirect_to(book_path)
+
+    notice = 'The book you were trying to find can not be found.'
+    expect(flash[:alert]).to eq notice
+  end
 end
